37eff5a3966e88fbc9f4f43b32ffb3307b8aec26
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2020-06-02 15:29:16 +0530

    first message

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwok;
+
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation for that word.
+ */
+public class Word {
+
+    /** Default translation for the word */
+    private String mDefaultTranslation;
+
+    /** Miwok translation for the word */
+    private String mkannadaTranslation;
+
+    private int mImageResourceId;
+
+    /** Audio resource ID for the word */
+    private int mAudioResourceId;
+
+    /** Image resource ID for the word */
+//    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    /** Constant value that represents no image was provided for this word */
+//    private static final int NO_IMAGE_PROVIDED = -1;
+
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultTranslation is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokTranslation is the word in the Miwok language
+     */
+    public Word(String defaultTranslation, String miwokTranslation,int audioresouceid) {
+        mDefaultTranslation = defaultTranslation;
+        mkannadaTranslation = miwokTranslation;
+        mAudioResourceId=audioresouceid;
+    }
+    public Word(String defaultTranslation, String miwokTranslation,int image,int audioresouceid) {
+        mDefaultTranslation = defaultTranslation;
+        mkannadaTranslation = miwokTranslation;
+        mImageResourceId=image;
+        mAudioResourceId=audioresouceid;
+
+    }
+
+    /**
+     * Get the default translation of the word.
+     */
+    public String getDefaultTranslation() {
+
+        return mDefaultTranslation;
+    }
+
+    /**
+     * Get the Miwok translation of the word.
+     */
+    public String getMiwokTranslation() {
+        return mkannadaTranslation;
+    }
+
+    /**
+     * Return the image resource ID of the word.
+     */
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    /**
+     * Returns whether or not there is an image for this word.
+     */
+//    public boolean hasImage() {
+//        return mImageResourceId != NO_IMAGE_PROVIDED;
+//    }
+
+    /**
+     * Return the audio resource ID of the word.
+     */
+    public int getAudioResourceId(){
+        return mAudioResourceId;
+    }
+
+}
